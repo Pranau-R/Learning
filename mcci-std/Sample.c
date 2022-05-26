c53d2c272d3ff11d075b99ee869093d957506107
PranauR <ajaypranauer10@gmail.com>
2022-05-26 13:46:25 +0530

    #3: re-arranged files

---
+++ mcci-std/Sample.c
@@ -0,0 +1,102 @@
+/* sample.c	Mon Feb 14 2005 15:17:17  cjy */
+
+/*
+
+Module:  sample.c
+
+Function:
+	Insert module explaination here
+
+Version:
+	V1.01a	Mon Feb 14 2005 15:17:17	cjy	Edit level 1
+
+Copyright notice:
+	This file copyright (C) 2005 by
+
+		MCCI Corporartion
+		3520 Krums Corners Road
+		Ithaca, NY 14850
+
+	An unpublished work.  All rights reserved.
+
+	This file is proprietary information, and may not be disclosed or
+	copied without the prior permission of MCCI Corporartion
+
+Author:
+	Chris Yokum, MCCI Corporartion	February 2005
+
+Revision history:
+   1.01a  Mon Feb 14 2005 15:17:17  cjy
+	Module created.
+
+*/
+
+#include <stdxxx.h>
+#include <CompuDAS.h>
+
+/****************************************************************************\
+|
+|		Manifest constants & typedefs.
+|
+|	This is strictly for private types and constants which will not
+|	be exported.
+|
+\****************************************************************************/
+
+xxx
+
+/****************************************************************************\
+|
+|	Read-only data.
+|
+|	If program is to be ROM-able, these must all be tagged read-only
+|	using the ROM storage class; they may be global.
+|
+\****************************************************************************/
+
+xxx
+
+/****************************************************************************\
+|
+|	VARIABLES:
+|
+|	If program is to be ROM-able, these must be initialized
+|	using the BSS keyword.  (This allows for compilers that require
+|	every variable to have an initializer.)  Note that only those
+|	variables owned by this module should be declared here, using the BSS
+|	keyword; this allows for linkers that dislike multiple declarations
+|	of objects.
+|
+\****************************************************************************/
+
+xxx
+
+/*
+
+Name:	SampleFunction
+
+Function:
+	Sample function one-line description.
+
+Definition:
+	void
+	SampleFunction(
+		int args
+		);
+
+Description:
+	Multi-line function description.
+
+Returns:
+	void
+
+*/
+void
+SampleFunction(
+    int args
+	)
+	{
+	code here
+	}
+
+
